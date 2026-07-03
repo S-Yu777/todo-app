a78ebcc6ded4c6a5c1e76da410166b657c4fbef0
Yu Sakai <yusakai@Mac.AirPort>
2026-07-03 14:07:11 +0900

    Todoアプリ完成（Django + CRUD + PostgreSQL + Bootstrap）

---
+++ todo/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Task
+
+def task_list(request):
+    if request.method == "POST":
+        title = request.POST.get("title")
+        if title:
+            Task.objects.create(title=title)
+        return redirect("task_list")
+
+    tasks = Task.objects.all()
+    return render(request, "todo/task_list.html", {"tasks": tasks})
+
+
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.delete()
+    return redirect("task_list")
+
+
+def toggle_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.done = not task.done
+    task.save()
+    return redirect("task_list")
