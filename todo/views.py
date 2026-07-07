f61186b4e9b13f15e71d5351309a22759ae4bfc4
Yu Sakai <yusakai@YunoMacBook-Air.local>
2026-07-07 13:11:33 +0900

    ログイン機能追加

--- todo/views.py
+++ todo/views.py
@@ -1,25 +1,58 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task
 
+
+@login_required
 def task_list(request):
+
     if request.method == "POST":
         title = request.POST.get("title")
+
         if title:
-            Task.objects.create(title=title)
+            Task.objects.create(
+                title=title,
+                user=request.user
+            )
+
         return redirect("task_list")
 
-    tasks = Task.objects.all()
-    return render(request, "todo/task_list.html", {"tasks": tasks})
+    tasks = Task.objects.filter(
+        user=request.user
+    )
+
+    return render(
+        request,
+        "todo/task_list.html",
+        {"tasks": tasks}
+    )
 
 
+@login_required
 def delete_task(request, task_id):
-    task = get_object_or_404(Task, id=task_id)
+
+    task = get_object_or_404(
+        Task,
+        id=task_id,
+        user=request.user
+    )
+
     task.delete()
+
     return redirect("task_list")
 
 
+@login_required
 def toggle_task(request, task_id):
-    task = get_object_or_404(Task, id=task_id)
-    task.done = not task.done
+
+    task = get_object_or_404(
+        Task,
+        id=task_id,
+        user=request.user
+    )
+
+    task.completed = not task.completed
+
     task.save()
+
     return redirect("task_list")
